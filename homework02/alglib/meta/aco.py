963183b4bd1b2ab2385cf01ae4eb4860be99a199
Milton Palacin <milton.palacin@gmail.com>
2020-06-23 21:00:39 -0500

    Control

--- homework02/alglib/meta/aco.py
+++ homework02/alglib/meta/aco.py
@@ -32,7 +32,7 @@ class AcoStaffing:
         self.__task_skill = task_skill
         self.__task_precedent = task_precedent
         self.__mind_strategy = mind_strategy
-        self.config(ants=20, alpha=0.5, beta=3, rho=0.5, tau=0.4, quu=0.5, generation=1000)
+        self.config(ants=20, alpha=0.5, beta=3, rho=0.5, tau=0.4, quu=0.5, generation=200)
         self.weight_config(wcost=10**(-6), wdur=10**(-1), wover=10**(-1))
 
     def config(self, ants=None, alpha=None, beta=None, rho=None, tau=None, quu=None, generation=None):
@@ -64,11 +64,12 @@ class AcoStaffing:
         # print("---", heuristic_values)
 
         # 04. Generar solución aleatoria
-        current_solution, goals = self.generate_initial_solution()
+        current_solution, current_goals = self.generate_initial_solution()
 
         print("Solucion inicial:")
-        print(current_solution)
-        print(goals)
+        # print(current_solution)
+        # print(current_goals)
+        self.result_print(current_solution, current_goals)
 
         last_ind = 0.0
         last_ind_new = 0.0
@@ -77,9 +78,9 @@ class AcoStaffing:
         iteration = 0
         max_wait = 100
         count_wait = 0
-        self.__generation = 1
-        self.__ants = 1
-
+        # self.__generation = 100
+        # self.__ants = 10
+        
         while (count_wait <= max_wait) and (iteration <= self.__generation):
             iteration += 1
             for _ in range(self.__ants):
@@ -96,112 +97,27 @@ class AcoStaffing:
 
                 candidate_solution = np.asarray([[self.__mind_strategy[np.argmax(node)][1] for node in staff] for staff in ant_values])
                 if self.assess_feasibility(candidate_solution):
-                    continue
-                print(30*"*", "candidate_solution")
-                print(candidate_solution)
-                print(30*"*", "compute_candidate_solution")
-                print(self.compute_candidate_solution(candidate_solution))
-        """
-        # 1. Generar todas las posibles interacciones basado en el total de valores que pueden tomar cada uno de los parámetros
-        #    V1^P1, V2^P2, V3^P3,..., Vn^Pn
-        uncovering_list = AcoTWay.initial_uncovering_list(self.__parameters, self.__t_way)
-
-        # 2. Iniciar la matriz de covertura
-        covering_list = []
-
-        # 3. Iniciar los principales argumentos del algoritmo
-        #    el número de iteración y el número de hormigas se han asignado en el constructor
-
-
-
-        # 4. Recorrer todos los casos de prueba generados  posibles
-        while len(uncovering_list) > 0:
-            # rutas candidatas
-            candidates_path = []
-
-            # 5. Iniciar matriz de feromonas con la constante ingresada _tau
-            pheromone = AcoTWay.start_matrix_values(self.__parameters, self.__tau)
-
-            # 6. Cálculo inicial de la heuristica para cada recorrido de la hormiga
-            heuristic = AcoTWay.compute_heuristic_values(self.__parameters, covering_list)
-
-            # Para mostrar un log en el caso quede pegado en un optimo local
-            ########### INI LOG ##################
-            end_force_close_time = time.process_time()
-            if end_force_close_time - ini_force_close_time > 60:
-                print("Caso donde falto completar la combinacion de alguna de sus variables:",uncovering_list)
-                break
-            log.debug_timer("Combinaciones NO cubiertas:", len(uncovering_list), ", Casos de prueba cubiertos:", len(covering_list), "indicador", last_ind)
-            last_ind_new = len(covering_list)/len(uncovering_list)
-            if(last_ind_new != last_ind):
-                ini_force_close_time = time.process_time()
-                last_ind = last_ind_new
-            ########### INI FIN ##################
-
-            # 7. Repetir/iterar el el recorrido de todas las hormigas N (pasado en __iteration) veces
-            for _ in range(self.__iteration):
-                # rutas de las hormigas
-                ants_path = []
-
-                # 8. Realizar el recorrido para cada hormiga hormigas
-                for _ in range(self.__ants):
-                    # crear la matriz de probabilidades para generar los edges (rutas hacia nodos/parámetro)
-                    var_proba = AcoTWay.start_matrix_values(self.__parameters, 0.0)
-
-                    # 9. Cada hormiga recorre los parámetros pasando por un ruta (selección de variable) para construir un caso de prueba
-                    for pos_param in range(len(self.__parameters)):
-
-                        # var_proba[pos_param] = AcoTWay.explore_probability(pheromone[pos_param], heuristic[pos_param], self.__alpha, self.__beta)
-                        # 10. Recorre cada nodo(parámetro), asigna probabilidad a los rutas (variable) por parámetro.
-                        #     utiliza el valor de qo para explotar un ruta existente o explorar un nuevo ruta (edge)
-                        #     esta sección servirá para eligir el ruta (variable) de mayor probabilidad
-                        quu = random.uniform(0, 1)
-                        if quu > self.__quu:
-                            # exploramos un nuevo ruta
-                            var_proba[pos_param] = AcoTWay.explore_probability(pheromone[pos_param], heuristic[pos_param], self.__beta)
-
-                        elif quu <= self.__quu:
-                            # explotamos un nuevo ruta
-                            var_proba[pos_param] = AcoTWay.exploit_probability(pheromone[pos_param], heuristic[pos_param], self.__alpha, self.__beta)
-
-                    # 11. Guardar la mejor de las rutas las rutas realizada por cada hormiga
-                    path = [np.argmax(v) for v in var_proba]
-                    ants_path.append(path)
-
-                    # 12. Evaporación de feromona: reducir su presencia por un factor 0<rho<=1. Actualización Local.
-                    #     y  iberación de feromonas: las hormigas liberan hormigas en las ruta que ha recorrido (los resumimos)
-                    pheromone = AcoTWay.local_pheromones_update(path, pheromone, self.__tau, self.__rho)
-
-                # 13. Seleccionar la mejor ruta (por el número de casos que cubre en las combinaciones tway) realizada por cada hormiga
-                best_ant_path = AcoTWay.fitness_function(uncovering_list, covering_list, ants_path)
-                candidates_path.append(best_ant_path)  # puede ser vacío, cauando el camino esta cubierto
-
-                # 14. Actualización global de feromonas. Solo se actualiza el mejor
-                if best_ant_path[0] != 0:
-                    pheromone = AcoTWay.global_pheromones_update(best_ant_path[1], pheromone, self.__rho, best_ant_path[0], len(uncovering_list))
-
-            # 15. Elegir el mejor de todos los candidatos
-            best_candidate = AcoTWay.max_candidate(candidates_path)
-
-            # 16. Agregar el mejor candidato a la lista de cobertura total.
-            if best_candidate[0] != 0:
-                covering_list.append(best_candidate[1])
-
-                # 17. Actualizar la lista de combinaciones no cubiertas.
-                for uncov in AcoTWay.cover_uncovering_list(uncovering_list, covering_list, self.__parameters):
-                    uncovering_list = tool.remove_array_array(uncovering_list, uncov)
-
-        # 18. Retornar la lista de covertura de los caso de prueba.
-        return np.asarray(covering_list)
-        """
+                    candidate_solution, candidate_goals = self.compute_candidate_solution(candidate_solution)
+                    if candidate_goals[0] > current_goals[0]:
+                        # print(30*"*", "ANTES")
+                        # print(current_solution, current_goals)
+                        current_solution = candidate_solution
+                        current_goals = candidate_goals
+                        # print(30*"*", "Ahora")
+                        # print(current_solution, current_goals)
+                        # Update feromona
+        print("Solucion Final:")
+        self.result_print(current_solution, current_goals)
+        # print(current_solution)
+        # print(current_goals)
 
     # FUNCIONES DE APOYO
+
     def exploit_ant_path(self, pheromone_values, heuristic_values):
         up = (pheromone_values ** self.__alpha) * (heuristic_values ** self.__beta)
         down = [[u if u > 0 else 1.0] for u in np.sum(up, axis=1)]
         random_exploit = np.asarray([np.asarray(np.random.uniform(0, 1, len(self.__mind_strategy))) for _ in range(len(self.__staff))])
         return random_exploit*(up/down)
-        return up/down
 
     def explore_ant_path(self, heuristic_values):
         up = (heuristic_values ** self.__beta)
@@ -247,21 +163,6 @@ class AcoStaffing:
 
             if self.assess_feasibility(solution_matrix):
                 break
-            # # Validando que cada tarea este a cargo de mínimo un empleado
-            # if np.min([np.sum(v) for _, v in enumerate(solution_matrix)]) > 0:
-            #     # Validando que las personas asignadas a una tarea cubran las habilidades requeridad (skills)
-            #     for i in range(len(self.__task)):
-            #         a = self.__task_skill[i, 1:]
-            #         c = [0 for _ in range(len(a))]
-            #         for j, val in enumerate(solution_matrix[i]):
-            #             if val > 0:
-            #                 b = self.__staff_skill[j, 1:]
-            #                 c = [int(c[k] or b[k]) for k in range(len(b))]
-            #         d = [int(c[k] and a[k]) for k in range(len(a))]
-            #         # print(a, c, d)
-            #         if np.sum(a) != np.sum(d):
-            #             continue
-            #     break
 
         return self.compute_candidate_solution(np.asarray(solution_matrix))
 
@@ -279,13 +180,25 @@ class AcoStaffing:
         # Calculando trabajo de sobretiempo en el staff y tareas
         project_overtime_task_total, project_overeffort_staff_total = self.project_over_calc(task_matrix_dur, tpg_scheduler, solution_matrix)
 
-        return solution_matrix, np.asarray([project_dur_total,
-                                            project_max_dur_total,
+        fitness_value = self.fitness_function(project_cost_total,
+                                              project_dur_total,
+                                              project_overtime_task_total,
+                                              project_overeffort_staff_total)
+
+        return solution_matrix, np.asarray([fitness_value,
                                             project_cost_total,
+                                            project_dur_total,
+                                            project_max_dur_total,
                                             project_overtime_task_total,
                                             project_overeffort_staff_total,
                                             np.asarray(task_matrix_dur),
                                             np.asarray(tpg_scheduler)])
+
+    def fitness_function(self, cost, duration, overtime, overeffort):
+        return (self.__wcost*cost +
+                self.__wdur*duration +
+                self.__wover*overtime +
+                self.__wover*overeffort)**(-1)
 
     def assess_feasibility(self, solution_matrix):
         # Validando que cada tarea este a cargo de mínimo un empleado
@@ -325,14 +238,15 @@ class AcoStaffing:
         project_dur_total = 0
         for i in range(len(self.__task)):
             precedents = self.__task_precedent[i, 1:]
+
             start = 0
             for j in range(len(self.__task)):
                 if precedents[j] == 1:
                     start = max(start, tpg_scheduler[j][1])
             end = start + task_matrix_dur[i]
             tpg_scheduler.append([start, end])
+            #print(i, precedents, start, end, tpg_scheduler)
             project_dur_total = end
-
         return project_dur_total, tpg_scheduler
 
     def project_max_duration_calc(self):
@@ -363,9 +277,10 @@ class AcoStaffing:
 
         # De personas
         # Mejorar el cálculo para actividade que se sobrepones, usando una unidad de tiempo (por ejemplo días)
+        tpg_scheduler_tmp = []
         project_overeffort_staff_total = 0
         for i, e in enumerate(self.__staff):
-            tpg_scheduler_tmp = tpg_scheduler[:]
+            tpg_scheduler_tmp = np.copy(tpg_scheduler)
             over_effort = 0
             for j in range(len(self.__task)):
                 end = tpg_scheduler_tmp[j][1]
@@ -379,7 +294,31 @@ class AcoStaffing:
             over_effort = over_effort - self.__task[i][2]
             # print(i, over_effort)
             project_overeffort_staff_total += 0 if over_effort <= 0 else over_effort
-
         return project_overtime_task_total, project_overeffort_staff_total
 
-# FIN:FUNCIONES DE APOYO
+    def result_print(self, solution, goal):
+        print(20*"*", 60*"*", 20*"*")
+        print(20*"*", 60*"*", 20*"*")
+        print(20*"*", 60*"*", 20*"*")
+        print(20*"*", "MATRIZ DE ASIGNACION", 20*"*")
+        print(solution)
+        print(20*"*", "VALOR FITNESS", 20*"*")
+        print(goal[0])
+        print(20*"*", "VALOR COST", 20*"*")
+        print(goal[1])
+        print(20*"*", "VALOR DURACION DEL PROYECTO", 20*"*")
+        print(goal[2])
+        print(20*"*", "VALOR DURACION MAXIMA", 20*"*")
+        print(goal[3])
+        print(20*"*", "SOBRETIEMPO DE TAREAS", 20*"*")
+        print(goal[4])
+        print(20*"*", "SOBRETIEMPO DE PERSONAL", 20*"*")
+        print(goal[5])
+        print(20*"*", "DURACION DE CADA TAREA", 20*"*")
+        print(goal[6])
+        print(20*"*", "RUTA CRITICA DE TAREAS (CRONOGRAMA)", 20*"*")
+        print(goal[7])
+        print(20*"*", 60*"*", 20*"*")
+        print(20*"*", 60*"*", 20*"*")
+        print(20*"*", 60*"*", 20*"*")
+        # FIN:FUNCIONES DE APOYO
